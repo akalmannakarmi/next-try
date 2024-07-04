0d6ced0cf053373f5085fe54ad966c41c134a5c0
akal <akalnakarmi1@gmail.com>
2024-07-04 18:43:32 +0545

    routing metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+	title: "About Page",
+};
+
 export default function about() {
 	return <h1>About Page</h1>
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,17 @@
+import { Metadata } from "next";
+
+type Props = {
+	params :{
+		productId:string,
+	};
+};
+
+export const generateMetadata = ({params}:Props) : Metadata => {
+	return {
+		title : `Product ${params.productId}`,
+	};
+}
+
 export default function product({params,}:{params:{productId:string}}) {
 	return <h1>Details about Product {params.productId}</h1>
 }
